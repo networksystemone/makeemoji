4a80e5fb524be8f096b378a8a3efb5cbc38e680f
Matt Popovich <mattpopovich@outlook.com>
2020-01-17 13:54:15 -0800

    basic typescript setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import "@babel/polyfill";
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+render(<App />, document.getElementById("app"));
 
 module.hot.accept();
