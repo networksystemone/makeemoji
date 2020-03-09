7a0375c27e08edf1df1e026da54de267abb53ea2
Matt Popovich <mattpopovich@outlook.com>
2020-03-08 20:25:03 -0700

    react: simplifying useResizedImage further and fixing ts strict check issues

--- src/components/App/useResizedImage.ts
+++ src/components/App/useResizedImage.ts
@@ -3,42 +3,27 @@ import { useState, useEffect } from "react";
 const SIZE: number = 100;
 const backgroundColor: string = "#ffffff";
 
-const createResizer = (canvas: HTMLCanvasElement) => {
-  return (
-    imgsrc: string, 
-    width: number, 
-    height: number, 
-    callback: Function
-  ) => {
-    let img = new Image();
-    img.src = imgsrc;
-    img.onload = () => {
-      canvas.width = width;
-      canvas.height = height;
-      const ctx: CanvasRenderingContext2D | null = canvas.getContext("2d");
-      if (!ctx) return;
-      ctx.fillStyle = backgroundColor;
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
-      ctx.drawImage( img, 0, 0, img.width, img.height, 0, 0, width, height);
-      callback(canvas.toDataURL("image/png"));
-    };
-  };
-};
-
 const useResizedImage = (src: string) => {
   const [resizedImage, setResizedImage] = useState('');
   
-  const passResized = (dataURL: string) => {
-    setResizedImage(dataURL);
-  };
-  
   useEffect(() => {
-    if (src) {
-      const resizer = createResizer(document.createElement("canvas"));
-      resizer(src, SIZE, SIZE, passResized);
-    } else {
+    if (!src) {
       setResizedImage('');
+      return;
     }
+    let img = new Image();
+    img.onload = () => {
+      const canvas = document.createElement("canvas");
+      canvas.width = SIZE;
+      canvas.height = SIZE;
+      const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+      ctx.fillStyle = backgroundColor;
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, SIZE, SIZE);
+      const dataUrl = canvas.toDataURL("image/png");
+      setResizedImage(dataUrl);
+    };
+    img.src = src;
   }, [src]);
 
   return resizedImage;
